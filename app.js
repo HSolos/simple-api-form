7a78bce4c5d9f264104396e411a062d436b8b1c1
Ronaldo <enigmagb9@gmail.com>
2023-09-23 10:54:30 -0500

    update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000; // Usa el puerto proporcionado por Azure 
 app.get("/", (req, res) => {
     res.send("Hola, Azure");
 });
+app.post("/", (req, res) => {
+    res.send("Post Method");
+});
 
 app.listen(port, () => {
     console.log(`La aplicación está en ejecución en el puerto ${port}`);
